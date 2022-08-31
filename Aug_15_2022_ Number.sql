48f6ace499aecf8e53c1c223a3019a2f5607371a
Zhen-Cheng <62698556+Jim0715@users.noreply.github.com>
2022-09-01 00:26:13 +0800

    Update Aug_15_2022_ Number.sql
    Add comment

--- Aug_15_2022_ Number.sql
+++ Aug_15_2022_ Number.sql
@@ -32,10 +32,8 @@ CREATE TABLE 編號測試表2
 GO
 INSERT INTO 編號測試表2(資料) VALUES('AAA');
 INSERT INTO 編號測試表2(資料) VALUES('BBB');
-INSERT INTO 編號測試表2(資料) VALUES('CCC');
+INSERT INTO 編號測試表2(全球唯一編號1,資料) VALUES(NEWID(),'CCC');
 INSERT INTO 編號測試表2(全球唯一編號1,資料) VALUES(NEWID(),'DDD');
-INSERT INTO 編號測試表2(全球唯一編號1,資料) VALUES(NEWID(),'EEE');
-INSERT INTO 編號測試表2(全球唯一編號1,資料) VALUES(NEWID(),'FFF');
 SELECT * FROM [編號測試表2]
 
 --若要將 uniqueidentifier 資料類型的資料行，設定其「預設值(Default)」為 0，也就是 00000000-0000-0000-0000-000000000000。
@@ -43,8 +41,8 @@ SELECT * FROM [編號測試表2]
 CREATE TABLE 編號測試表3
 (
 	自動編號 INT IDENTITY (1,1),
-	全球唯一編號1 UNIQUEIDENTIFIER DEFAULT NEWID(),            --舊 因數值南轅北轍
-	全球唯一編號2 UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID(),  --新
+	全球唯一編號1 UNIQUEIDENTIFIER DEFAULT NEWID(),            --舊 因數值南轅北轍 Creates a random GUID
+	全球唯一編號2 UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID(),  --新 Creates a sequential GUID
 	資料 NVARCHAR (10)
 )
 GO
