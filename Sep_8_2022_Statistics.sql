ea035998f99a324bab026c510e1aad494c46bf30
Zhen-Cheng <62698556+Jim0715@users.noreply.github.com>
2022-09-10 01:40:47 +0800

    Update Sep_8_2022_Statistics.sql
    Add HW code

--- Sep_8_2022_Statistics.sql
+++ Sep_8_2022_Statistics.sql
@@ -1,135 +1,145 @@
 /*
-1. 2003�~ �U���禬���B(�]�t�W��~�Z�B�릨��(Format�ʤ���))
+1. 2003年 各月營收金額(包含上月業績、月成長(Format百分比))
+SELECT C.訂單月份, C.業績, C.上個月業績,CONCAT((round(C.業績/C.上個月業績*100,2)),'%') AS 月成長
+FROM
+	(SELECT ROW_NUMBER() OVER (ORDER BY YEAR(A.訂單日期) ASC) AS 列號, YEAR(A.訂單日期) AS 訂單年份, MONTH(A.訂單日期) AS 訂單月份 ,SUM(B.金額) AS 業績,LAG(SUM(B.金額)) OVER (ORDER BY YEAR(A.訂單日期) ASC) AS 上個月業績
+	FROM 訂貨主檔 AS A JOIN 
+	(SELECT 訂單號碼,ROUND(SUM(單價*數量*(1+折扣)),0) AS 金額 FROM 訂貨明細 GROUP BY 訂單號碼) AS B
+	ON A.訂單號碼=B.訂單號碼
+	WHERE A.訂單日期 BETWEEN '2002-12-01' AND '2003-12-31' 
+	GROUP BY YEAR(A.訂單日期),MONTH(A.訂單日期))
+	AS C
+WHERE C.列號 BETWEEN '2' AND '13';
 
-2. �C��Ȥ᪺�����ʶR���j�Ѽ�
+2. 每位客戶的平均購買間隔天數
 
 
 https://docs.microsoft.com/zh-tw/sql/t-sql/functions/percentile-cont-transact-sql?view=sql-server-ver16
-3-1. ����P�����~�ꤤ���
-3-2. ���鲣�~����� 25% �����50% 75% 90%
-3-3. �U�����~����� 25% �����50% 75% 90%
+3-1. 全體同仁的薪資中位數
+3-2. 整體產品的售價 25% 中位數50% 75% 90%
+3-3. 各類產品的售價 25% 中位數50% 75% 90%
 
-4-1. �C����u�b��¾��s�����~��ʤ���
-4-2. �C�����~�b�������~�s��������ʤ���
+4-1. 每位員工在該職位群中的薪資百分位
+4-2. 每項產品在該類產品群中的價格百分位
 
-5-1. �U�����~(�C)�B�U�~(��) �� �P����B �ϯä��R
-5-2. �U����(�C)�B�U�����~(��) �� �P��ƶq �ϯä��R
+5-1. 各類產品(列)、各年(欄) 的 銷售金額 樞紐分析
+5-2. 各縣市(列)、各類產品(欄) 的 銷售數量 樞紐分析
 
 */
 
-USE ����_��
+USE 中文北風
 
 SELECT *
-FROM ( SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	   FROM ���u ORDER BY �~�� DESC) AS A
-ORDER BY �~�� ASC
+FROM ( SELECT 員工編號,姓名,稱呼,職稱,薪資
+	   FROM 員工 ORDER BY 薪資 DESC) AS A
+ORDER BY 薪資 ASC
 
 
 SELECT *
-FROM ( SELECT TOP(10) ���u�s��,�m�W,�٩I,¾��,�~��
-	   FROM ���u ORDER BY �~�� DESC) AS A
-ORDER BY �~�� ASC
+FROM ( SELECT TOP(10) 員工編號,姓名,稱呼,職稱,薪資
+	   FROM 員工 ORDER BY 薪資 DESC) AS A
+ORDER BY 薪資 ASC
 
---�p���M�涰  6-10
+--聯集和交集  6-10
 SELECT *
-FROM( SELECT TOP (10) ���u�s��,�m�W,�٩I,¾��,�~��
-	  FROM ���u ORDER BY �~�� DESC) AS A
+FROM( SELECT TOP (10) 員工編號,姓名,稱呼,職稱,薪資
+	  FROM 員工 ORDER BY 薪資 DESC) AS A
 
-EXCEPT        --INTERSECT(�涰) , EXCEPT(����) 
+EXCEPT        --INTERSECT(交集) , EXCEPT(扣除) 
 SELECT *
-FROM( SELECT TOP (5) ���u�s��,�m�W,�٩I,¾��,�~��
-	  FROM ���u ORDER BY �~�� DESC) AS B
-ORDER BY �~�� DESC;
+FROM( SELECT TOP (5) 員工編號,姓名,稱呼,職稱,薪資
+	  FROM 員工 ORDER BY 薪資 DESC) AS B
+ORDER BY 薪資 DESC;
 
 
 SELECT  A.*
-FROM( SELECT TOP (10) ���u�s��,�m�W,�٩I,¾��,�~��
-	  FROM ���u ORDER BY �~�� DESC) AS A
+FROM( SELECT TOP (10) 員工編號,姓名,稱呼,職稱,薪資
+	  FROM 員工 ORDER BY 薪資 DESC) AS A
 	LEFT JOIN
- (SELECT TOP (5) ���u�s��,�m�W,�٩I,¾��,�~��
-	  FROM ���u ORDER BY �~�� DESC) AS B 
-  ON A.���u�s��=B.���u�s��
- WHERE B.���u�s�� IS NULL;
+ (SELECT TOP (5) 員工編號,姓名,稱呼,職稱,薪資
+	  FROM 員工 ORDER BY 薪資 DESC) AS B 
+  ON A.員工編號=B.員工編號
+ WHERE B.員工編號 IS NULL;
 
  --0.420
- SELECT TOP(5) ���u�s��,�m�W,�٩I,¾��,�~��
- FROM ���u
- WHERE ���u�s�� NOT IN (SELECT TOP(5) ���u�s�� FROM ���u ORDER BY �~�� DESC)
- ORDER BY �~�� DESC;
+ SELECT TOP(5) 員工編號,姓名,稱呼,職稱,薪資
+ FROM 員工
+ WHERE 員工編號 NOT IN (SELECT TOP(5) 員工編號 FROM 員工 ORDER BY 薪資 DESC)
+ ORDER BY 薪資 DESC;
 
  SELECT *
- FROM (SELECT ���u�s��,�m�W,�٩I,¾��,�~��,
-		(SELECT COUNT(*) FROM ���u AS B WHERE B.�~��>A.�~��) AS �ƦW FROM ���u AS A) AS C
-WHERE �ƦW>=25 AND �ƦW<=29
-ORDER BY �~�� DESC;
+ FROM (SELECT 員工編號,姓名,稱呼,職稱,薪資,
+		(SELECT COUNT(*) FROM 員工 AS B WHERE B.薪資>A.薪資) AS 排名 FROM 員工 AS A) AS C
+WHERE 排名>=25 AND 排名<=29
+ORDER BY 薪資 DESC;
 
 --SQL 2005
---���������
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,ROW_NUMBER() OVER (ORDER BY �~�� DESC) AS �C��
-FROM ���u AS A
+--視窗型函數
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,ROW_NUMBER() OVER (ORDER BY 薪資 DESC) AS 列號
+FROM 員工 AS A
 
 SELECT * 
-FROM ( SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,ROW_NUMBER() OVER (ORDER BY �~�� DESC) AS �C��
-FROM ���u AS A) AS B
-WHERE �C��>=26 AND �C��<=30;
+FROM ( SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,ROW_NUMBER() OVER (ORDER BY 薪資 DESC) AS 列號
+FROM 員工 AS A) AS B
+WHERE 列號>=26 AND 列號<=30;
 
 
---�������A�ƧǮa��
---���B���ƪ������|���ۦP���ƧǡA�����򪺸�ƨä��|����Ƨ�
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,RANK() OVER (ORDER BY �~�� DESC) AS �ƦW
-FROM ���u AS A
+--視窗型，排序家族
+--金額重複的部分會有相同的排序，但後續的資料並不會接續排序
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,RANK() OVER (ORDER BY 薪資 DESC) AS 排名
+FROM 員工 AS A
 
---�����ƱƧǫ�A�|����Ƨ�
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,DENSE_RANK() OVER (ORDER BY �~�� DESC) AS �ƦW
-FROM ���u AS A 
+--不重複排序後，會接續排序
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,DENSE_RANK() OVER (ORDER BY 薪資 DESC) AS 排名
+FROM 員工 AS A 
 
---���~�����
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,NTILE(4) OVER (ORDER BY �~�� DESC) AS �s��
-FROM ���u AS A
+--依薪資分組
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,NTILE(4) OVER (ORDER BY 薪資 DESC) AS 群組
+FROM 員工 AS A
 
---ROW_NUMBER()  �q�Y�ƨ�� PARTITION BY �s��  ORDER BY �Ѥp��j�Ƨ�
-SELECT �Ȥ�s��,�q�渹�X
-	,ROW_NUMBER() OVER (PARTITION BY �Ȥ�s�� ORDER BY �q�渹�X) AS �y����
-FROM �q�f�D��;
+--ROW_NUMBER()  從頭排到尾 PARTITION BY 群組  ORDER BY 由小到大排序
+SELECT 客戶編號,訂單號碼
+	,ROW_NUMBER() OVER (PARTITION BY 客戶編號 ORDER BY 訂單號碼) AS 流水號
+FROM 訂貨主檔;
 
 --SQL2012
---�U��5�C���Ƨ�
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-FROM ���u
-ORDER BY �~�� DESC OFFSET 5 ROWS;
+--下移5列做排序
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+FROM 員工
+ORDER BY 薪資 DESC OFFSET 5 ROWS;
 
---�U��5�C���ƧǡA���e5�C
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-FROM ���u
-ORDER BY �~�� DESC OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
+--下移5列做排序，取前5列
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+FROM 員工
+ORDER BY 薪資 DESC OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
 
 
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,LAG(�~��) OVER (ORDER BY �~�� ASC) AS �e�@���~��
-	,�~��-LAG(�~��) OVER(ORDER BY �~�� ASC) AS �~��t
-	,LAG(�~��,2) OVER(ORDER BY �~�� ASC) AS �e����~��
-	,LAG(�~��,2,0) OVER(ORDER BY �~�� ASC) AS �e����~��
-FROM ���u AS A
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,LAG(薪資) OVER (ORDER BY 薪資 ASC) AS 前一位薪資
+	,薪資-LAG(薪資) OVER(ORDER BY 薪資 ASC) AS 薪資差
+	,LAG(薪資,2) OVER(ORDER BY 薪資 ASC) AS 前兩位薪資
+	,LAG(薪資,2,0) OVER(ORDER BY 薪資 ASC) AS 前兩位薪資
+FROM 員工 AS A
 
-SELECT ���u�s��,�m�W,�٩I,¾��,�~��
-	,LEAD(�~��) OVER (ORDER BY �~�� ASC) AS ��@���~��
-	,�~��-LEAD(�~��) OVER(ORDER BY �~�� ASC) AS �~��t
-	,LEAD(�~��,2) OVER(ORDER BY �~�� ASC) AS �����~��
-	,LEAD(�~��,2,0) OVER(ORDER BY �~�� ASC) AS �����~��
-FROM ���u AS A
+SELECT 員工編號,姓名,稱呼,職稱,薪資
+	,LEAD(薪資) OVER (ORDER BY 薪資 ASC) AS 後一位薪資
+	,薪資-LEAD(薪資) OVER(ORDER BY 薪資 ASC) AS 薪資差
+	,LEAD(薪資,2) OVER(ORDER BY 薪資 ASC) AS 後兩位薪資
+	,LEAD(薪資,2,0) OVER(ORDER BY 薪資 ASC) AS 後兩位薪資
+FROM 員工 AS A
 
---�ϯø�Ƨe�{
-SELECT �٩I,¾��,COUNT(*) �H��
-FROM ���u
-GROUP BY �٩I,¾��;
---- ������������
-SELECT [¾��],ISNULL([�p�j],0) AS [�p�j],[����]
-FROM (SELECT �٩I,¾��,COUNT(*) �H��
-FROM ���u
-GROUP BY �٩I,¾��) AS A
-PIVOT (SUM(�H��) FOR �٩I IN ([�p�j],[����])) AS P;
+--樞紐資料呈現
+SELECT 稱呼,職稱,COUNT(*) 人數
+FROM 員工
+GROUP BY 稱呼,職稱;
+--- ↓↓↓↓↓↓
+SELECT [職稱],ISNULL([小姐],0) AS [小姐],[先生]
+FROM (SELECT 稱呼,職稱,COUNT(*) 人數
+FROM 員工
+GROUP BY 稱呼,職稱) AS A
+PIVOT (SUM(人數) FOR 稱呼 IN ([小姐],[先生])) AS P;
