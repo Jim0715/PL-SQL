37103eff2ce81bf40da7ba7b20ca1a7b9ecb575e
Zhen-Cheng <62698556+Jim0715@users.noreply.github.com>
2022-10-27 19:33:34 +0800

    Update Oct_25_2022_ Memory_Table.sql

--- Oct_25_2022_ Memory_Table.sql
+++ Oct_25_2022_ Memory_Table.sql
@@ -3,6 +3,8 @@
 2. 等同 隔離等級 UNCOMMITTED
 3. 若需要交易行為或超高效能，請搭配「原生編譯」預存程序
 4. 記憶體表不支援「Truncate Table」，若需清空記憶體表資料，只能乖乖Delete
+
+5. 記憶體表沒版法製作叢集索引，記憶體表本身的資料就是亂的(記憶體)
 */
 
 
@@ -114,3 +116,65 @@ EXEC usp_InsertMemoryData @rowcount, @c;
 SET @timems = datediff(ms, @starttime, sysdatetime());
 SELECT 'memory-optimized table w/hash index and native SP:' + CAST(@timems as varchar(10)) + ' ms';
 
+-----------------------------------------------------------------------------------------------------------
+
+SELECT * FROM [SQL普通];	--11.11
+SELECT * FROM [記憶1];		--4.5
+SELECT * FROM [記憶2];		--4.5
+
+
+SELECT * FROM [SQL普通] WHERE [c1]=786543;		--0.00328
+SELECT * FROM [記憶1] WHERE [c1]=786543;		--0.0000175
+SELECT * FROM [記憶2] WHERE [c1]=786543;		
+
+
+SELECT * FROM [SQL普通] WHERE [c1]>=786541 AND [c1]<=786545;		--0.00328
+SELECT * FROM [記憶1] WHERE [c1]>=786541 AND [c1]<=786545;		--3.122
+SELECT * FROM [記憶2] WHERE [c1]>=786541 AND [c1]<=786545;		--3.122
+SELECT * FROM [記憶2] WHERE [c1] IN(786541,786542,786543,786544,786545);   --0.0825
+
+----------------------------------------------------------------------------------------
+DROP TABLE [dbo].[記憶1];
+
+CREATE TABLE [dbo].[記憶1] (
+  c1 INT NOT NULL PRIMARY KEY NONCLUSTERED,
+  c2 NCHAR(48) NOT NULL
+) WITH (MEMORY_OPTIMIZED=ON, DURABILITY = SCHEMA_AND_DATA);
+GO
+
+-----
+
+DECLARE @starttime datetime2 = sysdatetime();
+DECLARE @timems INT;
+DECLARE @i INT = 1
+DECLARE @rowcount INT = 1000000
+DECLARE @c NCHAR(48) = N'12345678901234567890123456789012345678'
+
+BEGIN TRAN
+  WHILE @i <= @rowcount
+    BEGIN
+      INSERT INTO [dbo].[記憶1](c1,c2) VALUES (@i, @c);
+      SET @i += 1;
+    END;
+COMMIT;
+
+SET @timems = datediff(ms, @starttime, sysdatetime());
+SELECT ' memory-optimized table w/ hash index and interpreted Transact-SQL: ' + CAST(@timems as VARCHAR(10)) + ' ms';
+
+
+SELECT * FROM [SQL普通];	--11.11
+SELECT * FROM [記憶1];		--4.5
+SELECT * FROM [記憶2];		--4.5
+
+SELECT * FROM [SQL普通] WHERE [c1]=786543;		--0.00328
+SELECT * FROM [記憶1] WHERE [c1]=786543;		--0.0004596
+SELECT * FROM [記憶2] WHERE [c1]=786543;		--0.0000175
+
+SELECT * FROM [SQL普通] WHERE [c1]>=786541 AND [c1]<=786545;	--0.00328
+SELECT * FROM [記憶1] WHERE [c1]>=786541 AND [c1]<=786545;		--0.000534
+SELECT * FROM [記憶2] WHERE [c1]>=786541 AND [c1]<=786545;		--3.122
+
+SELECT * FROM [SQL普通] WHERE [c1]>=786541 AND [c1]<=786545 ORDER BY [c1] ASC;	--0.00328
+SELECT * FROM [SQL普通] WHERE [c1]>=786541 AND [c1]<=786545 ORDER BY [c1] DESC;	--0.00328
+SELECT * FROM [記憶1] WHERE [c1]>=786541 AND [c1]<=786545 ORDER BY [c1] ASC;	--0.000534
+SELECT * FROM [記憶1] WHERE [c1]>=786541 AND [c1]<=786545 ORDER BY [c1] DESC;	--0.0119
